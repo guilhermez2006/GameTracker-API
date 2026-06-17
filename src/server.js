2f6fe0075b1db9e840edc93343eadf964d121f50
guilhermez2006 <guilhermefk123@gmail.com>
2026-06-16 21:34:04 -0300

    fix: corrijindo server.js

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ app.use(express.json());
 
 app.use(gamesRoutes);
 
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000 🚀");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
